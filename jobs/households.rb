da8d61a5ae8aadf66fe21aa1b81bd61c116c3d4f
tbrooke <tom.brooke@gmail.com>
2013-07-14 17:41:15 -0400

    set up households

--- jobs/households.rb
+++ jobs/households.rb
@@ -1,16 +1,8 @@
 require_relative '../lib/client'
 
 SCHEDULER.every '15s', allow_overlapping: false do |job|
-  patients = Msm::Client.summary
+  households = Msm::Client.summary
 
-  send_event('registrations-total', { current: patients['registrations_total'] })
-  send_event('registrations-today', { current: patients['registrations_today'] })
-
-  send_event('check-outs-total', { current: patients['check_outs_total'] })
-  send_event('check-outs-today', { current: patients['check_outs_today'] })
-
-  send_event('check-outs-per-hour',    { value: patients['check_outs_per_hour'] })
-  send_event('registrations-per-hour', { value: patients['registrations_per_hour'] })
-
-  send_event('patients-in-clinic', { current: patients['in_clinic'] })
+  send_event('households-count', { current: households['households_total'] })
+  
 end
