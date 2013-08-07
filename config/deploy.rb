91470b1d6b63c6a9695f179ee726976678673527
tbrooke <tom.brooke@gmail.com>
2013-08-07 15:44:17 -0400

    set up deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,22 +6,31 @@ require 'bundler/capistrano'
 
 set :application, "dashboard"
 
-set :repository, "git://github.com/mission-of-mercy/momma_dashboard.git"
+set :repository, "git@github.com:tbrooke/momma_dashboard.git"
 set :deploy_via, :remote_cache
 
 set :scm, :git
 set :user, "deploy"
 
-set :deploy_to, "/home/deploy/#{application}"
+set :deploy_to, "/var/www/#{application}"
 set :use_sudo, false
 
 after 'deploy:update_code' do
-  { "mom.yml"         => "config/mom.yml",
+  { "msm.yml"         => "config/mom.yml",
     "log"             => "log" }.
    each do |from, to|
      run "ln -nfs #{shared_path}/#{from} #{release_path}/#{to}"
    end
 end
+
+ desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
 
 after "deploy", "deploy:cleanup"
 
